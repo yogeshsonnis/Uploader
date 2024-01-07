1993ff71bbeb756c60eb4c0df4fa5f1f84f3f164
yogeshs <silverlightyogesh@gmail.com>
2024-01-07 14:08:16 +0530

    step editor, info editor ui, model, binding and styles implementation

--- Uploader/ViewModels/StepEditorVM.cs
+++ Uploader/ViewModels/StepEditorVM.cs
@@ -14,6 +14,55 @@ namespace Uploader.ViewModels
         public StepEditorVM(ProjectStep step)
         {
             this.step = step;
+            ConvertTpProjectStep(step);
+        }
+
+        private void ConvertTpProjectStep(ProjectStep step)
+        {
+            this.description = step.description;
+            this.audioFile = step.audioFile;
+            this.imageFile=step.imageFile;
+            this.videoFile=step.videoFile;
+        }
+
+         string _description;
+         string _audioFile;
+         string _imageFile;
+         string _videoFile;
+
+        public string audioFile
+        {
+            get { return _audioFile; }
+            set
+            {
+                _audioFile = value; OnPropertyChanged("introAudioFile");
+            }
+        }
+        public string description
+        {
+            get { return _description; }
+            set
+            {
+                _description = value; OnPropertyChanged("description");
+            }
+        }
+
+        public string imageFile
+        {
+            get { return _imageFile; }
+            set
+            {
+                _imageFile = value; OnPropertyChanged("extension1");
+            }
+        }
+
+        public string videoFile
+        {
+            get { return _videoFile; }
+            set
+            {
+                _videoFile = value; OnPropertyChanged("extension1AudioFile");
+            }
         }
     }
 }
